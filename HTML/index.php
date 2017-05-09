021b2b7980e3cbd6271982ff9d3e6e4de9c55369
Sam Martinez <sam.martinez.dev@gmail.com>
2017-05-08 21:43:03 -0500

    Converts project to php

---
+++ HTML/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title>Portfolio</title>
+
+		<meta charset = "utf-8">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" href="../Styles/style.css">
+	</head>
+
+	<body>
+		
+		<?php include("includes/header.html"); ?>
+		<?php include("includes/navigation.html"); ?>
+
+
+
+		<?php include("includes/footer.html"); ?>
+
+	</body>
+
+</html>
